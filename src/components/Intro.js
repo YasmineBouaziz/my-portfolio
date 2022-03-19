012d85d151899f7e4e2122799659676999b1d0f6
Yasmine Bouaziz <yasminebouaziz42@gmail.com>
2022-03-19 18:55:20 +0000

    fixed spelling mistake in intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
       <div className="intro-container">
         <Typewriter className="intro-title" text="Yasmine Bouaziz" />
         <div className="intro-description">
-        Hi, I'm Yasmine, an aspiring software engineer interested in full-stack development. I have recently completed a Bootcamp with Le Wagon in which I mostly worked with Ruby, Ruby on Rails and JavaScript. I'm currently undertaking a nanodegree with CodeFirstGirls that focuses on the fundamentals of computer science (data structures and algorithms) as well as Python and SQL. In my spare time, I'm working on a personal project Viewit, which is a Reddit clone that uses has a PostgreSQL, Python/ Flask backend and a React/ Javascript frontend.
+        Hi, I'm Yasmine, an aspiring software engineer interested in full-stack development. I have recently completed a Bootcamp with Le Wagon in which I mostly worked with Ruby, Ruby on Rails and JavaScript. I'm currently undertaking a nanodegree with CodeFirstGirls that focuses on the fundamentals of computer science (data structures and algorithms) as well as Python and SQL. In my spare time, I'm working on a personal project Viewit, which is a Reddit clone that uses PostgreSQL, has a Python/ Flask backend and a React/ Javascript frontend.
         </div>
       </div>
     );
